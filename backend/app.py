e43e6c9437ca9784e3d36132d093061c5e43dde4
Diego Bruno <diegobruno@MacBook-Air.local>
2025-09-08 15:07:04 -0300

    fix cors

--- backend/app.py
+++ backend/app.py
@@ -60,7 +60,7 @@ def create_app():
     
     # Configure CORS with more specific settings - incluye puerto 5174
     CORS(app, 
-         resources={r"/api/*": {"origins": ["http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173", "http://127.0.0.1:5174", "http://localhost:5001", "http://127.0.0.1:5001"]}}, 
+         resources={r"/api/*": {"origins": ["http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173", "http://127.0.0.1:5174", "http://localhost:5001", "http://127.0.0.1:5001","https://ananda-front.onrender.com" ]}}, 
          supports_credentials=True,
          allow_headers=["Content-Type", "Authorization"],
          methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
