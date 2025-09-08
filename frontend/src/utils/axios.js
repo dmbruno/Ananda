f5e4d1404f3a3d930474a45cb579f0a07697d1e0
Diego Bruno <diegobruno@MacBook-Air.local>
2025-09-08 14:37:58 -0300

    fix auth

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Configure axios defaults
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5001';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5001';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.timeout = 15000; // 15 seconds
 
